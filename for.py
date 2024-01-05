08a8e5486f0752f8e0e75d3ae2409540cf5e9609
khushbuanudip <110766719+khushbuanudip@users.noreply.github.com>
2024-01-05 17:31:50 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,26 @@
+
+
+
+for i in range(4,1,-1):
+    print("i=",i, end=" ")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print(" ")
+         
+
+list1=[12,34,50,27,5]
+
+
+
+    
+fruits = ["apple", "banana", "cherry"]
+colour=["Red","yellow","orange"]
+
+
+
+
+for i in range(len(fruits)):
+ 
+ for j in range(len(colour)):
+     print(fruits[i],colour[j])
+     
